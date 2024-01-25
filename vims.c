492342846feb371d95cbe2aab819716ea2e2bbff
Diya Alam <sougr32@gmail.com>
2024-01-25 22:53:50 +0600

    adding id genarator

--- vims.c
+++ vims.c
@@ -1,15 +1,18 @@
-#include<stdio.h>
-//#include <stdlib.h>
-//#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-struct Voter {
-    char name[50];
-    char address[100];
-    char phoneNumber[15];
+struct Voter
+{
+    char name[10];
+    char address[10];
+    char phoneNumber[12];
+    int id; 
 };
 
-void registerVoter(struct Voter *voter) {
-    printf("Enter your name: ");
+void registerVoter(struct Voter *voter)
+{
+    printf("\nEnter your name: ");
     scanf("%s", voter->name);
 
     printf("Enter your permanent address: ");
@@ -18,51 +21,62 @@ void registerVoter(struct Voter *voter) {
     printf("Enter your phone number: ");
     scanf("%s", voter->phoneNumber);
 
-    printf("Registration complete!.\n");
-    printf("Name: %s\nAddress: %s\nPhone Number: %s\n", voter->name, voter->address, voter->phoneNumber);
+    // Generating a random garbage value for ID
+    voter->id = rand();
+
+    printf("\nRegistration complete!\n");
+    printf("\nID: %d\nName: %s\nAddress: %s\nPhone Number: %s\n",voter->id, voter->name, voter->address, voter->phoneNumber);
+
+    exit(0); 
 }
 
-int main() {
-    struct Voter voters[100];
+int main()
+{
+    struct Voter voters[10];
     int numVoters = 0;
 
-    printf("Voter Management System");
+    printf("Voter Management System\n");
 
-    while (1) {
+    while (1)
+    {
         printf("\nMenu:\n\n");
         printf("1. Register as a voter\n");
         printf("2. Login or register as an administrator\n");
         printf("3. Exit\n");
 
         int choice;
-        printf("Your choice: ");
+        printf("\nYour choice: ");
         scanf("%d", &choice);
 
-        switch (choice) {
-            case 1:
-                if (numVoters < 100) {
-                    struct Voter newVoter;
-                    registerVoter(&newVoter);
-                    voters[numVoters] = newVoter;
-                    numVoters++;
-                } else {
-                    printf("Maximum number of voters reached.\n");
-                }
-                break;
-
-            case 2: {
-                // Administrator login and features 
-                break;
+        switch (choice)
+        {
+        case 1:
+            if (numVoters < 10)
+            {
+                struct Voter newVoter;
+                registerVoter(&newVoter);
+                voters[numVoters] = newVoter;
+                numVoters++;
             }
+            else
+            {
+                printf("Maximum number of voters reached.\n");
+            }
+            break;
 
-            case 3:
-                exit(0);
+        case 2:
+        {
+            // Administrator login and features
+            break;
+        }
 
-            default:
-                printf("Invalid choice. Please try again.\n");
+        case 3:
+            exit(0);
+
+        default:
+            printf("Invalid choice. Please try again.\n");
         }
     }
 
     return 0;
 }
-
